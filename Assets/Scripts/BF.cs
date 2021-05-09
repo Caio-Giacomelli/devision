e53cd88bce2cf47f70e9ca0a04604162265df21e
CHG1019 <caiogiacomelli.globant@sulamerica.com.br>
2021-05-08 23:31:14 -0300

    DEV - Added Pause/Play button to main game

--- Assets/Scripts/BF.cs
+++ Assets/Scripts/BF.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class BF : MonoBehaviour
@@ -8,7 +9,7 @@ public class BF : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,5 +24,35 @@ public class BF : MonoBehaviour
 
     public void Quit(){
         Application.Quit();
+    }
+
+    public void PauseUnpause(){
+        Sprite playSprite = Resources.Load<Sprite>("Sprites/Play");
+        Sprite pauseSprite = Resources.Load<Sprite>("Sprites/Pause");
+        
+        if (Time.timeScale == 0){
+            Time.timeScale = 1;
+
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+            
+            foreach(AudioSource a in audios){
+                a.Play(); 
+            }
+            
+
+            Button mybutton = GameObject.Find("Pause Button").GetComponent<Button>();
+            mybutton.image.sprite = pauseSprite;
+        }
+        else {
+            Time.timeScale = 0;
+
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+            
+            foreach(AudioSource a in audios){
+                a.Pause(); 
+            }
+            Button mybutton = GameObject.Find("Pause Button").GetComponent<Button>();
+            mybutton.image.sprite = playSprite;
+        }     
     }
 }
