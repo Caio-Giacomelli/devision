1e39e2f7dbdb5917112897634914b6eaf43d2b1d
CHG1019 <caiogiacomelli.globant@sulamerica.com.br>
2021-07-07 20:51:46 -0300

    DEV - Starting developing note destruction

--- Assets/Scripts/NoteLong.cs
+++ Assets/Scripts/NoteLong.cs
@@ -23,17 +23,20 @@ public class NoteLong : MonoBehaviour {
 
     public void setBarScale(float currentTime, float offset, float chartSpeed, float calibDelay, float fixedDelay, float activatorYposition){
 
+        if (this == null || this.gameObject == null) return; 
+        
         Transform noteTransform = this.gameObject.transform;        
 
         float yBase = activatorYposition - ((currentTime - (currentTime + offset)) * chartSpeed) + calibDelay + fixedDelay;
         float yCeiling = activatorYposition - ((currentTime - (_endTime + offset)) * chartSpeed) + calibDelay + fixedDelay;
 
         float yLongBarLocalScale = (yCeiling - yBase);
-        Debug.Log(message: $"yLongBarLocalScale: {yLongBarLocalScale}");
 
         if(yLongBarLocalScale >= 0){
             noteTransform.GetChild(0).localScale = new Vector3(0.2f, yLongBarLocalScale, 1f);
-            Debug.Log(message: $"Scale: {noteTransform.GetChild(0).localScale}");
+        } else {
+            Destroy(this);
+            Debug.Log(message: $"Destroyed!#@!@#!");
         }
  
     }
